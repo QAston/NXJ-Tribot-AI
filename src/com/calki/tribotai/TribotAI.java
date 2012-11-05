c6a6e09e2f25c978940b0d9e3e336f7397921e84
QAston <qaston@gmail.com>
2012-11-05 08:18:57 +0100

    Dodałem pliki projektu.

---
+++ src/com/calki/tribotai/TribotAI.java
@@ -0,0 +1,14 @@
+package com.calki.tribotai;
+
+import lejos.nxt.Button;
+
+public class TribotAI {
+
+	/**
+	 * @param args
+	 */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Button.waitForAnyPress();
+    }
+}
